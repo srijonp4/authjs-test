2788963420bdd9bce04cf4f47659d373f1d5cf00
Srijon Paul <86311499+srijonp4@users.noreply.github.com>
2024-03-01 01:15:43 +0530

    add: added skleton database setup implementing signup functionality

--- actions/register.ts
+++ actions/register.ts
@@ -2,12 +2,39 @@
 
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
+import { error } from 'console';
+import { getUserByEmail } from '@/data/user';
 /* Register Server Action */
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
-  console.log(validatedFields);
+
   if (!validatedFields.success) return { error: 'invalid feilds' };
 
-  return { success: 'success, verification email sent ' };
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'email already in use' };
+  }
+
+  try {
+    await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    //todo: send verification token to user
+    return { success: 'success, verification email sent ' };
+  } catch (err) {
+    console.error('Error creating user:', err);
+    return { error: 'Failed to create user' };
+  }
 };
